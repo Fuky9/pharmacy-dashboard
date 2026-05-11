53f933c42b16f57812361b2af5ba116ca9c4d06e
Fuky9 <lukasfukan6@gmail.com>
2026-05-11 23:01:25 +0200

    feat: initialize React frontend with Vite and API setup

---
+++ frontend/src/api/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_URL
+    : "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
